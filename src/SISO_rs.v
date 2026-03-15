6abb4533947dd1b2cd46a568c687150efe3cf204
YGDES <262647190+ygdes@users.noreply.github.com>
2026-03-15 15:44:42 +0100

    SISO_rs.v: conversion to RS finished (?)

--- src/SISO_rs.v
+++ src/SISO_rs.v
@@ -120,9 +120,9 @@ module siso_demux_mux_dl(
     output wire       Dout
 );
 
-  wire Even_odd, DevenN, DoddN, FbEven, FbOdd;
-//  wire Deven, Dodd;
-//  wire [3:0] LEneg, LOneg;
+  wire Even_odd, // FO3
+    DevenN, DoddN, Deven, Dodd; // FO5
+
   wire [3:0] Last_Unused;
   wire [3:0] te1, te2, te3, te1N, te2N, te3N;
   wire [3:0] to1, to2, to3, to1N, to2N, to3N;
@@ -133,25 +133,21 @@ module siso_demux_mux_dl(
   assign Latch_odd  = { Latch8[7], Latch8[5], Latch8[3], Latch8[1] };
 
 // slowdown 2x at the input
-// NOR all bits of Latch_odd:
+// NOR all bits of Latch_odd to dertermine parity:
   (* keep *) sg13g2_nor4_1 Nor_EvenOdd(.Y(Even_odd), .A(Latch8[1]), .B(Latch8[3]), .C(Latch8[5]), .D(Latch8[7]));
-  (* keep *) sg13g2_sdfrbp_1 sync_Deven(.Q(FbEven),  .Q_N(DevenN), .D(Din ),  .SCD(FbEven), .SCE(Even_odd), .RESET_B(RESET), .CLK(CLK));
-  (* keep *) sg13g2_sdfrbp_1 sync_Dodd (.Q(FbOdd),   .Q_N(DoddN ), .D(FbOdd), .SCD(Din  ),  .SCE(Even_odd), .RESET_B(RESET), .CLK(CLK));
-
-// Boost data pour les triangles d'entrée
-//  (* keep *) sg13g2_inv_4  Amp_evenN(.Y(Deven), .A(DevenN));
-//  (* keep *) sg13g2_inv_4  Amp_oddN (.Y(Dodd),  .A(DoddN));
+  (* keep *) sg13g2_sdfrbp_1 sync_Deven(.Q(Deven),  .Q_N(DevenN), .D(Din ),  .SCD(Deven), .SCE(Even_odd), .RESET_B(RESET), .CLK(CLK));
+  (* keep *) sg13g2_sdfrbp_1 sync_Dodd (.Q(Dodd),   .Q_N(DoddN ), .D(Dodd), .SCD(Din  ),  .SCE(Even_odd), .RESET_B(RESET), .CLK(CLK));
 
 // Le gros du délai : les 2 triangles dans un carré 4×4 chacun (odd+even)
-  siso_slice4_rs_pos slice0e(.siso_in({siso_last_even[3:1], FBeven}),     .siso_in_N({siso_last_even_N[3:1], DevenN}),      .siso_out(te1),             .siso_out_N(te1N),              .latch(Latch_even[3]));
-  siso_slice4_rs_pos slice1e(.siso_in({te1[3], te1[2], FBeven,  te1[0]}), .siso_in_N({te1N[3], te1N[2], DevenN,  te1N[0]}), .siso_out(te2),             .siso_out_N(te2N),              .latch(Latch_even[2]));
-  siso_slice4_rs_pos slice2e(.siso_in({te2[3], FBeven,  te2[1], te2[0]}), .siso_in_N({te2N[3], DevenN,  te2N[1], te2N[0]}), .siso_out(te3),             .siso_out_N(te3N),              .latch(Latch_even[1]));
-  siso_slice4_rs_pos slice3e(.siso_in({FBeven,  te3[2], te3[1], te3[0]}), .siso_in_N({DevenN,  te3N[2], te3N[1], te3N[0]}), .siso_out(siso_first_even), .siso_out_N(siso_first_even_N), .latch(Latch_even[0]));
+  siso_slice4_rs_pos slice0e(.siso_in({siso_last_even[3:1], Deven}),     .siso_in_N({siso_last_even_N[3:1], DevenN}),      .siso_out(te1),             .siso_out_N(te1N),              .latch(Latch_even[3]));
+  siso_slice4_rs_pos slice1e(.siso_in({te1[3], te1[2], Deven,  te1[0]}), .siso_in_N({te1N[3], te1N[2], DevenN,  te1N[0]}), .siso_out(te2),             .siso_out_N(te2N),              .latch(Latch_even[2]));
+  siso_slice4_rs_pos slice2e(.siso_in({te2[3], Deven,  te2[1], te2[0]}), .siso_in_N({te2N[3], DevenN,  te2N[1], te2N[0]}), .siso_out(te3),             .siso_out_N(te3N),              .latch(Latch_even[1]));
+  siso_slice4_rs_pos slice3e(.siso_in({Deven,  te3[2], te3[1], te3[0]}), .siso_in_N({DevenN,  te3N[2], te3N[1], te3N[0]}), .siso_out(siso_first_even), .siso_out_N(siso_first_even_N), .latch(Latch_even[0]));
 
-  siso_slice4_rs_pos slice0o(.siso_in({siso_last_odd [3:1], FBodd}),      .siso_in_N({siso_last_odd_N [3:1], DoddN}),       .siso_out(to1),             .siso_out_N(to1N),              .latch(Latch_odd[3]));
-  siso_slice4_rs_pos slice1o(.siso_in({to1[3], to1[2], FBodd,   to1[0]}), .siso_in_N({to1N[3], to1N[2], DoddN,   to1N[0]}), .siso_out(to2),             .siso_out_N(to2N),              .latch(Latch_odd[2]));
-  siso_slice4_rs_pos slice2o(.siso_in({to2[3], FBodd,   to2[1], to2[0]}), .siso_in_N({to2N[3], DoddN,   to2N[1], to2N[0]}), .siso_out(to3),             .siso_out_N(to3N),              .latch(Latch_odd[1]));
-  siso_slice4_rs_pos slice3o(.siso_in({FBodd,   to3[2], to3[1], to3[0]}), .siso_in_N({DoddN,   to3N[2], to3N[1], to3N[0]}), .siso_out(siso_first_odd),  .siso_out_N(siso_first_odd_N),  .latch(Latch_odd[0]));
+  siso_slice4_rs_pos slice0o(.siso_in({siso_last_odd [3:1], Dodd}),      .siso_in_N({siso_last_odd_N [3:1], DoddN}),       .siso_out(to1),             .siso_out_N(to1N),              .latch(Latch_odd[3]));
+  siso_slice4_rs_pos slice1o(.siso_in({to1[3], to1[2], Dodd,   to1[0]}), .siso_in_N({to1N[3], to1N[2], DoddN,   to1N[0]}), .siso_out(to2),             .siso_out_N(to2N),              .latch(Latch_odd[2]));
+  siso_slice4_rs_pos slice2o(.siso_in({to2[3], Dodd,   to2[1], to2[0]}), .siso_in_N({to2N[3], DoddN,   to2N[1], to2N[0]}), .siso_out(to3),             .siso_out_N(to3N),              .latch(Latch_odd[1]));
+  siso_slice4_rs_pos slice3o(.siso_in({Dodd,   to3[2], to3[1], to3[0]}), .siso_in_N({DoddN,   to3N[2], to3N[1], to3N[0]}), .siso_out(siso_first_odd),  .siso_out_N(siso_first_odd_N),  .latch(Latch_odd[0]));
 
 
 // Re-multiplexing
